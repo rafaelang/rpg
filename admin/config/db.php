1480a784e83fa8b2e697ce699714ea050dac4242
Rafael Garcia <rgarcia@raro.com.br>
2015-10-15 19:02:29 -0300

    admin, crud, models

--- admin/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=rpg',
-    'username' => 'root',
-    'password' => 'raro06',
-    'charset' => 'utf8',
-];

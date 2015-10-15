b4254aaace2d7d2504ffbde96b7e32baf5f6ab18
Rafael Garcia <rgarcia@raro.com.br>
2015-10-15 18:27:48 -0300

    admin

--- admin/config/db.php
+++ admin/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=rpg',
     'username' => 'root',
     'password' => 'raro06',
     'charset' => 'utf8',

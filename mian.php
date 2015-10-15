79cceedfc766522bb2620dee8792d7299cba575c
Rafael Garcia <rgarcia@raro.com.br>
2015-10-15 13:30:23 -0300

    add players

--- mian.php
+++ mian.php
@@ -1,4 +1,11 @@
 <?php
 require 'vendor/autoload.php';
 
+use Engine\Game;
+
 $game = new Game();
+
+$game->addPlayers(array(
+    new Player('Humam', 12, 1,2),
+    new Player('Orc', 20, 2,0),
+));
